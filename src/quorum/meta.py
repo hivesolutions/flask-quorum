326f1a02d6dc3199df51af95ecd8049f45c5c826
joamag <joamag@gmail.com>
2014-05-08 17:04:35 +0100

    comparision

--- src/quorum/meta.py
+++ src/quorum/meta.py
@@ -54,3 +54,9 @@ class Ordered(type):
 
     def __init__(cls, name, bases, attrs):
         super(Ordered, cls).__init__(name, bases, attrs)
+
+    def __cmp__(self, value):
+        return cmp(self.__name__, value.__name__)
+
+    def __lt__(self, value):
+        return self.__name__.__lt__(value.__name__)
