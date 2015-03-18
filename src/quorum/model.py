c7e2876fa121c79971d54bb5ee31a8a6425acc7b
joamag <joamag@gmail.com>
2015-03-18 21:53:54 +0000

    new pre-condition support

--- src/quorum/model.py
+++ src/quorum/model.py
@@ -552,6 +552,9 @@ class Model(legacy.with_meta(meta.Ordered, observer.Observable)):
                 if name.startswith("_"): continue
                 if not name == name.lower(): continue
                 if not isinstance(value, dict): continue
+                if name in definition: raise exceptions.OperationalError(
+                    "Duplicated attribute '%s' in '%s' hierarchy" % (name, _cls.__name__)
+                )
                 definition[name] = value
 
         # sets the "default" definition for the based identifier
