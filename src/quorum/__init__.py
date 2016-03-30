cd1df94a5973b75d2ec631442ea1ae09657318f1
joamag <joamag@gmail.com>
2016-03-30 22:52:49 +0100

    new validation exception support

--- src/quorum/__init__.py
+++ src/quorum/__init__.py
@@ -111,9 +111,9 @@ from .util import is_iterable, request_json, get_field, get_object, is_mobile, r
     get_locale, get_langs, set_locale, reset_locale, anotate_async, run_thread,\
     camel_to_underscore, generate_identifier, to_locale, nl_to_br, nl_to_br_jinja,\
     sp_to_nbsp, sp_to_nbsp_jinja, date_time, quote, unquote, JSONEncoder
-from .validation import validate, validate_b, safe, eq, gt, gte, lt, lte, not_null, not_empty,\
-    not_false, is_in, is_simple, is_email, is_url, is_regex, field_eq, field_gt, field_gte,\
-    field_lt, field_lte, string_gt, string_lt, string_eq, equals, not_past, not_duplicate,\
+from .validation import validate, validate_b, validate_e, safe, eq, gt, gte, lt, lte, not_null,\
+    not_empty, not_false, is_in, is_simple, is_email, is_url, is_regex, field_eq, field_gt,\
+    field_gte, field_lt, field_lte, string_gt, string_lt, string_eq, equals, not_past, not_duplicate,\
     all_different, no_self
 
 from .amazon import get_connection as get_amazon
