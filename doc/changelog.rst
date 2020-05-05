eea7ffcfc6fb3829e4cb67e766e84c636b623e04
joamag <joamag@hive.pt>
2020-05-05 09:06:03 +0100

    Version 0.5.36

--- doc/changelog.rst
+++ doc/changelog.rst
@@ -6,6 +6,11 @@ List the complete set of changes to the quorum project since it's creation.
 Current Versions
 ----------------
 
+0.5.36
+^^^^^
+
+    * Fixed issue with pypy
+
 0.5.35
 ^^^^^
 
--- src/quorum/info.py
+++ src/quorum/info.py
@@ -38,7 +38,7 @@ __license__ = "Apache License, Version 2.0"
 """ The license for the module """
 
 NAME = "quorum"
-VERSION = "0.5.35"
+VERSION = "0.5.36"
 AUTHOR = "Hive Solutions Lda."
 EMAIL = "development@hive.pt"
 DESCRIPTION = "Quorum Extensions for Flask"
